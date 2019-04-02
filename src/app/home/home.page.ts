729710935af7be713632b249d352f1bd7fed4c54
tsuyoshiyamamoto <t.yamamoto@tyotto.co.jp>
2019-04-02 11:49:07 +0900

    記事一覧画面の修正（データ取得処理は実装途中）

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  // constructor (public http: HttpClient) {}
+
+  ngOnInit() {
+
+  }
   // articleUrl: string;
   //
   // articleUrl = '';
