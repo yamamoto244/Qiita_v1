5a9b59f798bb9d929d533ecd88d6a91ac72c7cff
tsuyoshiyamamoto <t.yamamoto@tyotto.co.jp>
2019-04-05 10:07:24 +0900

    ion-infinite-scrollの実装

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+const itemPerPage = 10; // 1ページあたりに含まれる要素数
+const itemPage = 1; // ページ番号
 
 @Component({
   selector: 'app-home',
@@ -9,39 +11,74 @@ import { map } from 'rxjs/operators';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
   names: any;
   qiitaItems: Object;
-  constructor (public http: HttpClient) {
+  perPage = itemPerPage;
+  page = itemPage;
 
-    this.http.get('https://qiita.com/api/v2/items?page=1&per_page=20')
-    // .pipe(
-    //     map(res => res)
-    // )
+  constructor (
+      public http: HttpClient,
+  ) {
+
+    /*
+    this.http.get('https://qiita.com/api/v2/items?page=1&per_page=10')
         .subscribe(res => this.qiitaItems = res );
+        */
+    this.loadData(this.page, this.perPage);
   }
 
-  doRefresh(event) {
-    console.log('Begin async operation');
-    this.http.get('https://qiita.com/api/v2/items?page=1&per_page=20')
-        .subscribe(res => {
-          this.qiitaItems = res;
-        });
-
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      event.target.complete();
-    }, 2000);
+  doRefresh($event: any) {
+    this.page = itemPage;
+    this.perPage = itemPerPage;
+    this.loadData(this.page, this.perPage).then(() => {
+      $event.target.complete();
+    }, () => {
+      $event.target.complete();
+    });
+    // console.log('Begin async operation');
+    // this.http.get('https://qiita.com/api/v2/items?page=1&per_page=10')
+    //     .subscribe(res => {
+    //       this.qiitaItems = res;
+    //     });
+    // setTimeout(() => {
+    //   console.log('Async operation has ended');
+    //   event.target.complete();
+    // });
   }
 
-  getArticle() {
-    this.http.get('https://qiita.com/api/v2/items?page=1&per_page=5')
-    // .pipe(
-    //     map(res => res)
-    // )
-        .subscribe(res => {
-          this.qiitaItems = res;
-          console.log('res:', res);
-        });
+
+  loadOldData($event) {
+    this.perPage +=  itemPerPage;
+    this.loadData(this.page, this.perPage).then(() => {
+      $event.target.complete();
+    }, () => {
+      $event.target.complete();
+    });
+  }
+
+  private loadData(page: number, perPage: number): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      this.http.get('https://qiita.com/api/v2/items?page=' + page + '&per_page=' + perPage)
+          .subscribe(  res => {
+        this.qiitaItems = res;
+        resolve();
+      });
+    });
+
+    /*
+        try {
+          this.http.get('https://qiita.com/api/v2/items?page=1&per_page=10')
+              .subscribe( async  res => {
+                this.qiitaItems = await res;
+              });
+          return Promise.resolve();
+        } catch (e) {
+          return Promise.reject(e);
+        }
+        */
   }
 
   getTagList(tags) {
